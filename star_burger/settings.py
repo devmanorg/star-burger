b516ff906358f8f4698a323f310535529407f715
Alexander Samokhin <samokhin@shockland.ru>
2022-09-25 17:18:11 +0000

    Exclude static files

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
 API_YANDEX_TOKEN = env('API_YANDEX_TOKEN')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['.localhost', '127.0.0.1', '[::1]'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
