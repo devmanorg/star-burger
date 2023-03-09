bc97f786fe4619bb01437afbf4b8de933e444159
mavel <p.m.berdnikov@gmail.com>
2023-03-09 23:16:46 +0300

    extend data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,12 +1,11 @@
-import json
-
 from django.http import JsonResponse
 from django.templatetags.static import static
+from phonenumber_field.phonenumber import PhoneNumber
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Product, Order, ProductOrder
+from .models import Product, Order
 
 
 def banners_list_api(request):
@@ -63,12 +62,49 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-
-    if not isinstance(request.data.get('products'), list) or not request.data.get('products'):
+    required_fields = ['products', 'firstname', 'lastname', 'phonenumber', 'address']
+    if not all([request.data.get(field) for field in required_fields]):
         return Response(
-            {'error': 'products not provided or not a list'},
+            {'error': f'missing one or more required fields: {", ".join(required_fields)}'},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+    required_product_fields = ['product', 'quantity']
+    for product in request.data['products']:
+        if not all([product.get(field) for field in required_product_fields]):
+            return Response(
+                {'error': f'each product must have these int fields: {", ".join(required_product_fields)}'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    if not isinstance(request.data.get('products'), list):
+        return Response(
+            {'error': 'products must be a list'},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    str_fields = ['firstname', 'lastname', 'phonenumber', 'address']
+    if not all([isinstance(request.data[field], str) for field in str_fields]):
+        return Response(
+            {'error': 'wrong data type for one or more string fields'},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if not PhoneNumber.from_string(request.data['phonenumber']).is_valid():
+        return Response(
+            {'error': 'invalid phone number'},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    for product in request.data['products']:
+        try:
+            Product.objects.get(pk=product.get('product'))
+        except Product.DoesNotExist:
+            return Response(
+                {'error': 'product with the specified id does not exist'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
 
     request_products = request.data.pop('products')
     order = Order.objects.create(**request.data)
