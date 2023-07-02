581cbced7d2e1c2c9ca6510f2fca80d64ea7ee1b
mavel <p.m.berdnikov@gmail.com>
2023-07-02 23:14:05 +0200

    add guard checks for missing components, move script call to container

--- scripts/first_deploy.sh
+++ scripts/first_deploy.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+if [ ! -f data/db_dump.json ]; then
+    echo "Error: data/db_dump.json does not exist"
+    exit 1
+fi
+
+if [ ! -d media ] || [ ! "$(ls -A media)" ]; then
+    echo "Error: media directory does not exist or is empty"
+    exit 1
+fi
+
 cp starburger.service.template /etc/systemd/system/starburger.service
 systemctl daemon-reload
 systemctl enable starburger
@@ -10,4 +20,4 @@ systemctl start starburger
 docker exec star-burger-django-1 python manage.py migrate
 docker exec star-burger-django-1 python manage.py loaddata data/db_dump.json
 
-python scripts/report_deploy_rollbar.py
+docker exec star-burger-django-1 python scripts/report_deploy_rollbar.py
