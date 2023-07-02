707c22c5ad450c195f2455771d5e7df57d1370a4
mavel <p.m.berdnikov@gmail.com>
2023-07-02 19:35:21 +0200

    fix broken commands

--- scripts/first_deploy.sh
+++ scripts/first_deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 cp starburger.service.template /etc/systemd/system/starburger.service
 systemctl daemon-reload
 systemctl enable starburger
@@ -8,4 +10,4 @@ systemctl start starburger
 docker exec star-burger-django-1 python manage.py migrate
 docker exec star-burger-django-1 python manage.py loaddata data/db_dump.json
 
-venv/bin/python scripts/report_deploy_rollbar.py
+python scripts/report_deploy_rollbar.py
