eb51ba028c60cd0f19366d0ec041ab36fd6ac0a8
mavel <p.m.berdnikov@gmail.com>
2023-07-06 00:42:13 +0200

    remove user input check

--- scripts/init_letsencrypt.sh
+++ scripts/init_letsencrypt.sh
@@ -76,7 +76,8 @@ docker compose run --rm --entrypoint "\
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal" certbot
+    --force-renewal \
+    --no-eff-email" certbot
 echo
 
 echo "### Reloading nginx ..."
