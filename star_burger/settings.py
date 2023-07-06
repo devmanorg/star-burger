cd87f75c8a553cf66c39c5d60d5edc5581795dd6
mavel <p.m.berdnikov@gmail.com>
2023-07-06 12:21:55 +0200

    fix an error in path joining

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -88,7 +88,7 @@ MEDIA_URL = '/media/'
 if not env.str('POSTGRES_USER', None):
     DEFAULT_DATABASE = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 else:
     DEFAULT_DATABASE = {
